1f8c2b3676407bbf334c494bb44886a84158876e
Neha Arora <aroraneha009@gmail.com>
2026-01-02 12:41:35 +0530

    Add files via upload

---
+++ performance-benchmark/scripts/leiden-gpu.py
@@ -0,0 +1,76 @@
+import nx_cugraph as nxcg
+import subprocess
+import networkx as nx
+import time
+def load_graph(edge_list)-> nx.Graph:
+  G=nx.Graph()
+  graph=G.add_edges_from(edge_list)
+  return graph
+
+def leiden()-> None :
+    """
+    This module finds the communities using Louvain algorithm.
+
+    Arguments:
+      graph: Graph Object
+
+    Return:
+     None
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--edge", required=True, help="edge_list")
+    args = parser.parse_args()
+
+    graph = load_graph(args.edge)
+    G=nxcg.from_networkx(graph)
+
+    try:
+        result = subprocess.run(
+            ["nvidia-smi"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(result.stdout)
+    except FileNotFoundError:
+        print("nvidia-smi not found. Is NVIDIA driver installed?")
+
+    partition = list(nxcg.community.leiden_communities(G))
+    
+    node_to_comm={}
+    for com_id, node in enumerate(partition):
+        for node in nodes:
+           node_to_comm[node]=comm_id
+    node_colors = [node_to_comm[node] for node in G.nodes()]
+
+    plt.figure(figsize=(8, 6))
+    pos = nx.spring_layout(G, seed=42)
+
+    nx.draw(
+     G,
+     pos,
+     with_labels=True,
+     node_color=node_colors,
+     cmap=plt.cm.tab20,   # better than rainbow
+     node_size=500,
+     font_size=9
+    )
+
+    plt.title("Louvain Communities (nx-cugraph backend)")
+    plt.show()
+
+    try:
+        result = subprocess.run(
+            ["nvidia-smi"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(result.stdout)
+    except FileNotFoundError:
+        print("nvidia-smi not found. Is NVIDIA driver installed?")
+
+start_time=time.time()
+leiden() 
+end_time=time.time()
+print(end_time-start_time)
